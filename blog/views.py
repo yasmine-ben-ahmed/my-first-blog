a637e86bec010e0e01a299e9deed7a4b28673713
yasmine-ben-ahmed <benahmedyasmin@gmail.com>
2025-06-05 20:35:38 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all().order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) 
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -110,8 +110,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+""" STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static' """
+
+STATIC_URL = '/static/'
+
+# For production (like on PythonAnywhere)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
