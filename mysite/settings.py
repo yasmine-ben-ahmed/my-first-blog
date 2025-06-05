42cbaa9243623883cf71af2b47ea3e4a949d9519
yasmine-ben-ahmed <benahmedyasmin@gmail.com>
2025-06-05 21:03:06 +0100

    adding static folder.

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,12 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-""" STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static' """
+
 
 STATIC_URL = '/static/'
 
-# For production (like on PythonAnywhere)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
